71f7cd803757eaac47f5cdb1de075d7adee4f88d
AayushBade14 <aayushrajubade@gmail.com>
2025-08-02 19:54:16 +0530

    Created A Basic Renderer & Window Management

---
+++ Main.cpp
@@ -0,0 +1,184 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include "./Include/Shader/Shader.hpp"
+
+#define WIDTH 1920.0f
+#define HEIGHT 1013.0f
+#define TITLE "Furify"
+
+// ================[CAMERA-PROPERTIES]============================
+glm::vec3 cameraPos = {0.0f,0.0f,3.0f};
+glm::vec3 cameraFront = {0.0f,0.0f,-1.0f};
+glm::vec3 cameraUp = {0.0f,1.0f,0.0f};
+
+float cameraSpeed = 5.0f;
+float fov = 45.0f;
+float yaw = -90.0f;
+float pitch = 0.0f;
+float lastX = WIDTH/2.0f;
+float lastY = HEIGHT/2.0f;
+
+bool firstMouse = true;
+float dpi = 0.1f;
+
+float dt = 0.0f;
+float lastFrame = 0.0f;
+// ==============================================================
+
+// ==================[CALLBACKS]=================================
+void scroll_callback(GLFWwindow* window, double xOffset, double yOffset);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+// ===============================================================
+
+// ===================[METHODS]===================================
+void ProcessInput(GLFWwindow* window){
+  if(glfwGetKey(window,GLFW_KEY_ESCAPE)==GLFW_PRESS)
+    glfwSetWindowShouldClose(window,true);
+
+  if(glfwGetKey(window,GLFW_KEY_T)==GLFW_PRESS)
+    glPolygonMode(GL_FRONT_AND_BACK,GL_LINE);
+  if(glfwGetKey(window,GLFW_KEY_Y)==GLFW_PRESS)
+    glPolygonMode(GL_FRONT_AND_BACK,GL_FILL);
+
+  if(glfwGetKey(window,GLFW_KEY_W)==GLFW_PRESS)
+    cameraPos += cameraSpeed * cameraFront * dt;
+  if(glfwGetKey(window,GLFW_KEY_S)==GLFW_PRESS)
+    cameraPos -= cameraSpeed * cameraFront * dt;
+  if(glfwGetKey(window,GLFW_KEY_A)==GLFW_PRESS)
+    cameraPos -= cameraSpeed * glm::normalize(glm::cross(cameraFront,cameraUp)) * dt;
+  if(glfwGetKey(window,GLFW_KEY_D)==GLFW_PRESS)
+    cameraPos  += cameraSpeed * glm::normalize(glm::cross(cameraFront,cameraUp)) * dt;
+}
+// ===============================================================
+
+int main(void){
+  
+  // ==============[INITIALIZATION]=================
+  if(glfwInit()<0){
+    std::cerr<<"ERROR: GLFW_INIT!"<<std::endl;
+    return -1;
+  }
+
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR,6);
+  glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
+
+  GLFWwindow* window = glfwCreateWindow(WIDTH,
+                                        HEIGHT,
+                                        TITLE,
+                                        NULL,
+                                        NULL);
+  if(!window){
+    glfwTerminate();
+    std::cerr<<"ERROR: Creating Window!"<<std::endl;
+    return -1;
+  }
+
+  glfwMakeContextCurrent(window);
+
+  if(!gladGLLoader((GLADloadproc)glfwGetProcAddress)){
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    std::cerr<<"ERROR: GLAD_INIT!"<<std::endl;
+    return -1;
+  }
+
+  // ==============================================
+
+
+  // ============[SETTING CALLBACKS]==============
+  glfwSetInputMode(window,GLFW_CURSOR,GLFW_CURSOR_DISABLED);
+  glfwSetFramebufferSizeCallback(window,framebuffer_size_callback);
+  glfwSetCursorPosCallback(window,mouse_callback);
+  glfwSetScrollCallback(window,scroll_callback);
+  // ==============================================
+  
+  // ===================[OGL BUFFERS]===============
+  glEnable(GL_DEPTH_TEST);
+  // ===============================================
+  
+  // =================[RENDER-LOOP]=================
+
+  while(!glfwWindowShouldClose(window)){
+    // ----Calculating dt and processing inputs----
+    float currentFrame = (float)glfwGetTime();
+    dt = currentFrame - lastFrame;
+    lastFrame = currentFrame;
+    
+    ProcessInput(window);
+    // -----------------------------------------
+    
+    // ----------- Clearing Buffers -----------
+    glClearColor(0.0f,0.0f,0.0f,1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    // ----------------------------------------
+    
+
+    // -------------- Rendering ---------------
+
+    // ----------------------------------------
+    
+    // ------------- Swap buffers -------------
+    glfwPollEvents();
+    glfwSwapBuffers(window);
+    // ----------------------------------------
+  }
+
+  // ===============================================
+  
+  // ================[CLEANUP]======================
+  glfwTerminate();
+  glfwDestroyWindow(window);
+  // ===============================================
+  
+  return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height){
+  glViewport(0,0,width,height);
+}
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos){
+  float Xpos = (float)xpos;
+  float Ypos = (float)ypos;
+
+  if(firstMouse){
+    lastX = Xpos;
+    lastY = Ypos;
+    firstMouse = false;
+  }
+
+  float xOffset = Xpos - lastX;
+  float yOffset = lastY - Ypos;
+
+  xOffset *= dpi;
+  yOffset *= dpi;
+
+  lastX = Xpos;
+  lastY = Ypos;
+
+  yaw += xOffset;
+  pitch += yOffset;
+
+  if(pitch > 89.0f)
+    pitch = 89.0f;
+  if(pitch < -89.0f)
+    pitch = -89.0f;
+
+  glm::vec3 direction;
+  direction.x = glm::cos(glm::radians(pitch)) * glm::cos(glm::radians(yaw));
+  direction.y = glm::sin(glm::radians(pitch));
+  direction.z = glm::cos(glm::radians(pitch)) * glm::sin(glm::radians(yaw));
+
+  cameraFront = glm::normalize(direction);
+}
+
+void scroll_callback(GLFWwindow* window, double xOffset, double yOffset){
+  fov -= (float)yOffset;
+  if(fov > 45.0f)
+    fov = 45.0f;
+  if(fov < 1.0f)
+    fov = 1.0f;
+}
+
